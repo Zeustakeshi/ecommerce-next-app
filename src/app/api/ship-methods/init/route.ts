8c9a9c8bdfdce92b2c0736c5374ca6a63dcaa8ab
Zeustakeshi <gaconmvp2312@gmail.com>
2024-01-15 17:37:45 +0700

    fix Unique constraint failed on the fields: () and Tailwind: To use responsive styles you must have a <head> element as a direct child of the Tailwind component.

--- src/app/api/ship-methods/init/route.ts
+++ src/app/api/ship-methods/init/route.ts
@@ -1,16 +1,16 @@
 import db from "@/lib/db";
-import { ShipMethod } from "@prisma/client";
+import { ShipMethod, ShipMethodCode } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 const shipMethods: ShipMethod[] = [
     {
-        code: "G_EXPRESS",
+        code: ShipMethodCode.G_EXPRESS,
         name: "GExpress",
         description:
             "GExpress là đối tác vận chuyển chính thức của tdmu eStore, đảm bảo cung cấp dịch vụ giao hàng nhanh chóng, tiết kiệm và phù hợp với túi tiền của sinh viên.",
     },
     {
-        code: "SELLER_SHIPPING",
+        code: ShipMethodCode.SELLER_SHIPPING,
         name: "Người bán tự vận chuyển",
         description:
             "Phương thức vận chuyển này cho phép người bán hàng tự chịu trách nhiệm và tự vận chuyển sản phẩm đến địa chỉ của bạn. Thông tin chi tiết sẽ được cung cấp bởi người bán.",
--- src/email-templates/ProductRegisterRequest.tsx
+++ src/email-templates/ProductRegisterRequest.tsx
@@ -35,94 +35,106 @@ const ProductRegisterRequest = ({ product, shop }: Props) => {
         <Html>
             <Tailwind>
                 <Head />
-                <Preview>
-                    Yêu cầu đăng ký kinh doanh sản phẩm {product.name}{" "}
-                </Preview>
-                <Body style={main}>
-                    <Container style={container}>
-                        <Section>
-                            <Column>
-                                <Img
-                                    style={sectionLogo}
-                                    src={`${baseUrl}/logo.png`}
-                                    width="155"
-                                    height="31"
-                                    alt="TDMU eStore"
-                                />
-                            </Column>
-                        </Section>
+                <>
+                    <Preview>
+                        Yêu cầu đăng ký kinh doanh sản phẩm {product.name}{" "}
+                    </Preview>
+                    <Body style={main}>
+                        <Container style={container}>
+                            <Section>
+                                <Column>
+                                    <Img
+                                        style={sectionLogo}
+                                        src={`${baseUrl}/logo.png`}
+                                        width="155"
+                                        height="31"
+                                        alt="TDMU eStore"
+                                    />
+                                </Column>
+                            </Section>
 
-                        <Section style={paragraphContent}>
-                            <Hr style={hr} />
-                            <Text style={heading}>
-                                Yêu cầu đăng ký sản phẩm
-                            </Text>
-                            <Text style={paragraph}>
-                                Xin chào Admin TDMU eStore,
-                            </Text>
-                            <Text style={paragraph}>
-                                Chúng tôi muốn thông báo rằng có một yêu cầu
-                                đăng ký sản phẩm mới từ một người bán trên nền
-                                tảng của bạn.
-                            </Text>
-                        </Section>
+                            <Section style={paragraphContent}>
+                                <Hr style={hr} />
+                                <Text style={heading}>
+                                    Yêu cầu đăng ký sản phẩm
+                                </Text>
+                                <Text style={paragraph}>
+                                    Xin chào Admin TDMU eStore,
+                                </Text>
+                                <Text style={paragraph}>
+                                    Chúng tôi muốn thông báo rằng có một yêu cầu
+                                    đăng ký sản phẩm mới từ một người bán trên
+                                    nền tảng của bạn.
+                                </Text>
+                            </Section>
 
-                        <Section style={paragraphContent}>
-                            <Text style={paragraph} className="font-semibold">
-                                Thông tin Sản phẩm:
-                            </Text>
-                            <ul className="p-0">
-                                <li>
-                                    <Text style={paragraph}>
-                                        Tên sản phẩm: {product.name}
-                                    </Text>
-                                </li>
-                                <li>
-                                    <Text style={paragraph}>
-                                        Tên cửa hàng: {shop.name}
-                                    </Text>
-                                </li>
-                                <li>
-                                    <Text style={paragraph}>
-                                        Địa chỉ email: {shop.email}
-                                    </Text>
-                                </li>
-                            </ul>
-                        </Section>
-                        <Section style={paragraphContent}>
-                            <Text style={paragraph} className="font-semibold">
-                                Mô tả sản phẩm:{" "}
-                            </Text>
-                            <Text style={paragraph}>{product.description}</Text>
-                        </Section>
+                            <Section style={paragraphContent}>
+                                <Text
+                                    style={paragraph}
+                                    className="font-semibold"
+                                >
+                                    Thông tin Sản phẩm:
+                                </Text>
+                                <ul className="p-0">
+                                    <li>
+                                        <Text style={paragraph}>
+                                            Tên sản phẩm: {product.name}
+                                        </Text>
+                                    </li>
+                                    <li>
+                                        <Text style={paragraph}>
+                                            Tên cửa hàng: {shop.name}
+                                        </Text>
+                                    </li>
+                                    <li>
+                                        <Text style={paragraph}>
+                                            Địa chỉ email: {shop.email}
+                                        </Text>
+                                    </li>
+                                </ul>
+                            </Section>
+                            <Section style={paragraphContent}>
+                                <Text
+                                    style={paragraph}
+                                    className="font-semibold"
+                                >
+                                    Mô tả sản phẩm:{" "}
+                                </Text>
+                                <Text style={paragraph}>
+                                    {product.description}
+                                </Text>
+                            </Section>
 
-                        <Section style={paragraphContent}>
-                            <Button
-                                className="text-center px-5 py-2 rounded-md bg-blue-600 text-white font-semibold text-lg inline-block"
-                                href={`${baseUrl}/manager/admin/products/pending`}
-                            >
-                                Xem ngay
-                            </Button>
-                        </Section>
+                            <Section style={paragraphContent}>
+                                <Button
+                                    className="text-center px-5 py-2 rounded-md bg-blue-600 text-white font-semibold text-lg inline-block"
+                                    href={`${baseUrl}/manager/admin/products/pending`}
+                                >
+                                    Xem ngay
+                                </Button>
+                            </Section>
 
-                        <Section style={paragraphContent}>
-                            <Text style={paragraph}>Ảnh minh họa: </Text>
-                            <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
-                                {product.images.map((url, index) => {
-                                    return (
-                                        <Img
-                                            key={index}
-                                            className="w-full h-full object-cover"
-                                            src={url}
-                                            alt={`ảnh minh họa ${index + 1}`}
-                                        />
-                                    );
-                                })}
-                            </div>
-                            <Hr style={hr} />
-                        </Section>
-                    </Container>
-                </Body>
+                            <Section style={paragraphContent}>
+                                <Text style={paragraph}>Ảnh minh họa: </Text>
+                                <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+                                    {product.images.map((url, index) => {
+                                        return (
+                                            <Img
+                                                key={index}
+                                                className="w-full h-full object-cover"
+                                                src={url}
+                                                alt={`ảnh minh họa ${
+                                                    index + 1
+                                                }`}
+                                            />
+                                        );
+                                    })}
+                                </div>
+                                <Hr style={hr} />
+                            </Section>
+                        </Container>
+                    </Body>
+                </>
             </Tailwind>
         </Html>
     );
